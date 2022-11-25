d40208731770687b4a1b24e3c51252c717586bad
Alexy Da Silva <alexy.dasilva@icloud.com>
2022-11-25 15:56:55 +0100

    third commit

--- index.php
+++ index.php
@@ -10,8 +10,119 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
     </head>
-     <body>
+    <body>
         <h1>Hello, world!</h1>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+        
+        <img src="Images/6168680.png"/>
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary">A</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">B</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">C</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">D</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">E</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">F</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">G</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">H</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">I</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">J</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary">K</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">L</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">M</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">N</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">O</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">P</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">Q</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">R</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">S</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">T</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary">U</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">V</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">W</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">X</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">Y</button>
+            </div>
+
+            <div class="col">
+                <button type="button" class="btn btn-primary">Z</button>
+            </div>
+        </div>
+    </body>
 </html>
