6ffe2aced3bf9c6ee8997b9f9d4cea776ce8ccc6
MARINI Noé <n.marini@sio.local>
2022-11-25 17:43:47 +0100

    prout

--- index.php
+++ index.php
@@ -11,7 +11,35 @@
         <link rel="stylesheet" href="style.css">
     </head>
      <body>
-        <h1>Hello, world!</h1>
+
+     
+     
+        <div class="container">
+   
+        <div class="col-8 txtTitre fs-1">Le Pendu</div>
+
+
+
+
+    <div class="row">
+   
+        <div class="col-4 test"><img src="Images/Pendu.png"></div>
+        <div class="col-4 vide"></div>
+        <div class="col-4 test">test</div>
+
+
+</div>
+
+<div class="col-sm-8 txtTitre ">text ramdom</div>
+    
+</div>
+</div>
+
+
+
+
+
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
